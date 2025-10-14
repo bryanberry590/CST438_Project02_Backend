859fe871b755e801b5809105f9a9cf64faf24af1
DJLoya <dloya@csumb.edu>
2025-10-14 13:52:54 -0700

    renamed file

--- src/main/java/com/project02/springboot/controller/loginController.java
+++ src/main/java/com/project02/springboot/controller/loginController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.project02.springboot.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
